69648b4cf3b3c13832ca3e2d1b2542d76a1e89e7
Marcel J <marcelljee@gmail.com>
2017-08-01 15:52:28 +0700

    make score circle view into custom view

---
+++ app/src/main/java/co/marcelje/hackernews/view/ScoreView.java
@@ -0,0 +1,38 @@
+package co.marcelje.hackernews.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+
+import co.marcelje.hackernews.R;
+
+public class ScoreView extends AppCompatTextView {
+
+    private static final int TEXT_SIZE = 14;
+
+    public ScoreView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public ScoreView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public ScoreView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        setBackgroundResource(R.drawable.ic_circle);
+        setGravity(Gravity.CENTER);
+        setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
+        setTextSize(TEXT_SIZE);
+    }
+}
