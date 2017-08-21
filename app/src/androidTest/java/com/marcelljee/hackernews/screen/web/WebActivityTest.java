6d1af9267d2580071ee5a18c3e6726e7b128ffc6
Marcel J <marcelljee@gmail.com>
2017-08-21 20:06:08 +0700

    add Espresso test for WebActivity

---
+++ app/src/androidTest/java/com/marcelljee/hackernews/screen/web/WebActivityTest.java
@@ -0,0 +1,65 @@
+package com.marcelljee.hackernews.screen.web;
+
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.marcelljee.hackernews.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class WebActivityTest {
+
+    private static final String URL = "https://github.com/marcelljee/hacker-news-android-app";
+
+    @Rule
+    public final IntentsTestRule<WebActivity> intentsTestRule =
+            new IntentsTestRule<>(WebActivity.class, false, false);
+
+    @Before
+    public void setUp() {
+        Intent intent = new Intent();
+        intent.putExtra(WebActivity.EXTRA_URL, URL);
+        intentsTestRule.launchActivity(intent);
+    }
+
+    @Test
+    public void testToolbarActionMenu_clickShareMenuItem() {
+        openActionBarOverflowOrOptionsMenu(intentsTestRule.getActivity());
+        onView(withText(intentsTestRule.getActivity().getString(R.string.menu_item_share)))
+                .perform(click());
+
+        intended(allOf(
+                hasAction(Intent.ACTION_CHOOSER),
+                hasExtra(Intent.EXTRA_TITLE, intentsTestRule.getActivity().getString(R.string.title_share)),
+                hasExtraWithKey(Intent.EXTRA_INTENT)
+        ));
+    }
+
+    @Test
+    public void testToolbarActionMenu_clickOpenInBrowserMenuItem() {
+        openActionBarOverflowOrOptionsMenu(intentsTestRule.getActivity());
+        onView(withText(intentsTestRule.getActivity().getString(R.string.menu_item_open_in_browser)))
+                .perform(click());
+
+        intended(allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(URL)
+        ));
+    }
+}
