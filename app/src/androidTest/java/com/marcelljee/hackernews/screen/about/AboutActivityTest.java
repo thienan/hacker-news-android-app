0917f2b279e5b9a847fca8164115fda33371f459
Marcel J <marcelljee@gmail.com>
2017-08-21 21:42:41 +0700

    add Espresso test for NewsActivity

--- app/src/androidTest/java/com/marcelljee/hackernews/screen/about/AboutActivityTest.java
+++ app/src/androidTest/java/com/marcelljee/hackernews/screen/about/AboutActivityTest.java
@@ -1,9 +1,12 @@
 package com.marcelljee.hackernews.screen.about;
 
-import android.support.test.rule.ActivityTestRule;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.marcelljee.hackernews.R;
+import com.marcelljee.hackernews.chrome.CustomTabsBrowser;
+import com.marcelljee.hackernews.screen.web.WebActivity;
 import com.marcelljee.hackernews.utils.ItemUtils;
 
 import org.junit.Rule;
@@ -11,22 +14,47 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.*;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.core.AllOf.*;
 
 @RunWith(AndroidJUnit4.class)
 public class AboutActivityTest {
 
     @Rule
-    public final ActivityTestRule<AboutActivity> mActivityRule = new ActivityTestRule<>(AboutActivity.class);
+    public final IntentsTestRule<AboutActivity> intentsTestRule = new IntentsTestRule<>(AboutActivity.class);
 
     @Test
     public void testShowAboutText() {
         onView(withId(R.id.tv_about))
-                .check(matches(withText(ItemUtils.fromHtml(mActivityRule.getActivity(),
-                        mActivityRule.getActivity().getString(R.string.about_text)).toString())));
+                .check(matches(withText(ItemUtils.fromHtml(intentsTestRule.getActivity(),
+                        intentsTestRule.getActivity().getString(R.string.about_text)).toString())));
 
         onView(withId(R.id.tv_about))
                 .check(matches(hasLinks()));
+    }
+
+    @Test
+    public void testClickLink() {
+        onView(withId(R.id.tv_about))
+                .perform(openLinkWithText("Github page"));
+
+        String packageName = CustomTabsBrowser.getCustomTabPackageName(intentsTestRule.getActivity());
+
+        if (packageName == null) {
+            intended(allOf(
+                    hasComponent(WebActivity.class.getName()),
+                    hasExtra(WebActivity.EXTRA_URL, "https://github.com/marcelljee/hacker-news-android-app")
+            ));
+        } else {
+            intended(allOf(
+                    hasAction(Intent.ACTION_VIEW),
+                    hasData("https://github.com/marcelljee/hacker-news-android-app"),
+                    toPackage(packageName)
+            ));
+        }
     }
 }
---
+++ app/src/androidTest/java/com/marcelljee/hackernews/screen/news/NewsActivityTest.java
@@ -0,0 +1,133 @@
+package com.marcelljee.hackernews.screen.news;
+
+import android.content.Intent;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.marcelljee.hackernews.R;
+import com.marcelljee.hackernews.chrome.CustomTabsBrowser;
+import com.marcelljee.hackernews.screen.about.AboutActivity;
+import com.marcelljee.hackernews.screen.espresso.CustomViewActions;
+import com.marcelljee.hackernews.screen.settings.SettingsActivity;
+import com.marcelljee.hackernews.screen.web.WebActivity;
+import com.marcelljee.hackernews.utils.SettingsUtils;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.*;
+
+@RunWith(AndroidJUnit4.class)
+public class NewsActivityTest {
+
+    @Rule
+    public final IntentsTestRule<NewsActivity> intentsTestRule =
+            new IntentsTestRule<>(NewsActivity.class);
+
+    @Test
+    public void testToolbarActionMenu_checkClearHistoryMenuItemIsShown() {
+        onView(withId(R.id.spinner_news_type)).perform(click());
+        onData(allOf(
+                is(instanceOf(String.class)),
+                is(intentsTestRule.getActivity().getString(R.string.news_type_history))
+        )).perform(click());
+        onView(withId(R.id.action_clear_history)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.spinner_news_type)).perform(click());
+        onData(allOf(
+                is(instanceOf(String.class)),
+                is(intentsTestRule.getActivity().getString(R.string.news_type_top))
+        )).perform(click());
+        onView(withId(R.id.action_clear_history)).check(doesNotExist());
+    }
+
+    @Test
+    public void testToolbarActionMenu_checkShowMenuItemIsShown() {
+        onView(withId(R.id.spinner_news_type)).perform(click());
+        onData(allOf(
+                is(instanceOf(String.class)),
+                is(intentsTestRule.getActivity().getString(R.string.news_type_history))
+        )).perform(click());
+
+        onView(withId(R.id.action_show_all)).check(doesNotExist());
+        onView(withId(R.id.action_show_unread)).check(doesNotExist());
+
+        onView(withId(R.id.spinner_news_type)).perform(click());
+        onData(allOf(
+                is(instanceOf(String.class)),
+                is(intentsTestRule.getActivity().getString(R.string.news_type_top))
+        )).perform(click());
+
+        if (SettingsUtils.readIndicatorEnabled(intentsTestRule.getActivity())) {
+            onView(withId(R.id.action_show_all)).check(doesNotExist());
+            onView(withId(R.id.action_show_unread)).check(matches(isDisplayed()));
+
+            onView(withId(R.id.action_show_unread)).perform(click());
+
+            onView(withId(R.id.action_show_all)).check(matches(isDisplayed()));
+            onView(withId(R.id.action_show_unread)).check(doesNotExist());
+
+            onView(withId(R.id.action_show_all)).perform(click());
+
+            onView(withId(R.id.action_show_all)).check(doesNotExist());
+            onView(withId(R.id.action_show_unread)).check(matches(isDisplayed()));
+        } else {
+            onView(withId(R.id.action_show_all)).check(doesNotExist());
+            onView(withId(R.id.action_show_unread)).check(doesNotExist());
+        }
+    }
+
+    @Test
+    public void testToolbarActionMenu_clickAboutMenuItem() {
+        openActionBarOverflowOrOptionsMenu(intentsTestRule.getActivity());
+        onView(withText(intentsTestRule.getActivity().getString(R.string.menu_item_about)))
+                .perform(click());
+
+        intended(hasComponent(AboutActivity.class.getName()));
+    }
+
+    @Test
+    public void testToolbarActionMenu_clickSettingsMenuItem() {
+        openActionBarOverflowOrOptionsMenu(intentsTestRule.getActivity());
+        onView(withText(intentsTestRule.getActivity().getString(R.string.menu_item_settings)))
+                .perform(click());
+
+        intended(hasComponent(SettingsActivity.class.getName()));
+    }
+
+    @Test
+    public void testShareItem_onClickScoreView() {
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,
+                        CustomViewActions.clickChildViewWithId(R.id.sv_score)));
+
+        onView(withId(R.id.action_share)).perform(click());
+
+        intended(allOf(
+                hasAction(Intent.ACTION_CHOOSER),
+                hasExtra(Intent.EXTRA_TITLE, intentsTestRule.getActivity().getString(R.string.title_share)),
+                hasExtraWithKey(Intent.EXTRA_INTENT)
+        ));
+    }
+}
